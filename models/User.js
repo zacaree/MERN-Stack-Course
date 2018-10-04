e5dc71b4e71d7af336336dee1147ecd45cc3e616
Zac <zacaree@gmail.com>
2018-10-04 13:00:44 -0500

    Built a user schema

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Set module.exports to a variable, User
+// then set mongoose.model, passing in the name we want to use which is users
+// then the second parameter is the actual schema (above)
+module.exports = User = mongoose.model("users", UserSchema);
