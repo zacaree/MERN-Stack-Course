6726f913dc752e39e6fec0e0a6651af578b840e2
zacaree <zacharem@gmail.com>
2018-10-06 08:54:47 -0500

    Finished validation for user registration

--- validation/register.js
+++ validation/register.js
@@ -5,11 +5,38 @@ module.exports = function validateRegisterInput(data) {
   // errors begins as an empty object. If it remains empty till the end of the fn, all is well and the key isValid will resolve as true.
   let errors = {};
 
+  // Is the name input empty? If it's not, store it in this object. If it is make it an empty string.
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
   // Validator checks if the length is between 2 and 30.
   // If this isn't true then a string describing the error is added to the errors object.
   // This means the errors object is no longer empty.
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = "Name must be between 2 and 30 characters.";
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name is required";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
+  }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
   }
 
   // The fn returns an object with the errors object,
