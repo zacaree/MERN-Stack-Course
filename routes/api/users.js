cf09cbbf3cc3ca9111f965d79af65e6178438c47
Zac <zacaree@gmail.com>
2018-10-09 16:30:01 -0500

    My front-end is officially talking to my back-end!!! WOOHOOOOO!!! YEAH!

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,7 +20,7 @@ const User = require("../../models/User");
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Users works!" }));
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @desc    Register the user
 // @access  Public
 // It'll be router.post because we're going to expect a post request
