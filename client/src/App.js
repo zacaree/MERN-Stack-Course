f28c90dc5e6457159ed3a3a547bb57eeee0f9fbf
Zac <zacaree@gmail.com>
2018-10-09 14:52:14 -0500

    Added 2 more pages to the app and wired everything up with React Router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div class="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div class="container">
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
