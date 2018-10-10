c414f7c9fd610d9d1a8d740481a609edc843af15
zacaree <zacharem@gmail.com>
2018-10-10 09:08:52 -0500

    Improving registration validation

--- validation/register.js
+++ validation/register.js
@@ -17,28 +17,35 @@ module.exports = function validateRegisterInput(data) {
   // If this isn't true then a string describing the error is added to the errors object.
   // This means the errors object is no longer empty.
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = "Name must be between 2 and 30 characters";
+    errors.name = "Name must be at least 2 characters and no more than 30 characters";
   }
+
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name is required";
   }
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email is required";
-  }
+
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
   }
-  if (Validator.isEmpty(data.password)) {
-    errors.password = "Password is required";
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
   }
+
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = "Password must be at least 6 characters";
   }
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm password field is required";
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password is required";
   }
+
   if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
   }
 
   // The fn returns an object with the errors object,
